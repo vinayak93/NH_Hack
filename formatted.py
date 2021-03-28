81eef704799ea63449bba4d58d9d090edd546d05
bw1109 <54919990+bw1109@users.noreply.github.com>
2021-03-28 09:42:07 -0400

    Add files via upload

--- formatted.py
+++ formatted.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from tkinter import ttk
 from difflib import SequenceMatcher
 import pandas as pd
+from PIL import ImageTk, Image
+
 
 class Win(tk.Frame):
     def __init__(self, master):
@@ -18,26 +20,37 @@ class Win(tk.Frame):
         self.s.theme_use('alt')
 
         self.colors = ['red', 'green', 'blue', 'orange']
-        self.mainf.configure(background='orange')
+        self.mainf.configure(background='light blue')
         self.s.theme_use('clam')
 
         some_input = "test input widget"
         self.some_entry = tk.Entry(self.mainf, textvariable=some_input, width=18)
-        self.some_entry.insert(0, "Enter the fact")
-        self.some_entry.grid(row=1, column=2, padx=10, pady=10)
+        self.some_entry.insert(0, "Please enter the text...")
+        self.some_entry.grid(row=1, column=2, padx=10, pady=30)
         # self.some_entry.place(x=20, y=30)
-
+        self.img = Image.open("hackathon.png")
+        self.image1 = self.img.resize((120, 60), Image.ANTIALIAS)
         self.my_string_var = tk.StringVar()
+
+        self.load = Image.open("unh.jpeg")
+        self.loads = self.load.resize((120, 60), Image.ANTIALIAS)
+        self.render = ImageTk.PhotoImage(self.loads)
+
+
+        # labels can be text or images
+        self.img = ttk.Label(self.mainf, image=self.render)
+        self.img.image = self.render
+        self.img.place(x=385, y=260)
         print("1")
 
         def similar(a, b):
             return SequenceMatcher(None, a, b).ratio()
 
         def result_1():
-            self.my_string_var.set("result1")
+            self.my_string_var.set("Training complete")
 
         def result_2():
-            self.my_string_var.set("result2")
+            self.my_string_var.set("Hoax-BUSTER Prediction:"+ "result2")
 
         def result_3():
             data = pd.read_csv("total_news.csv")
@@ -48,16 +61,24 @@ class Win(tk.Frame):
                 if score > 0.6:
                     label_list = list(data.loc[[i]]["Label"])
                     print(label_list[0])
-                    self.my_string_var.set(str(label_list[0]))
+                    if (str(label_list[0]) == "true") or (str(label_list[0]) == "mostly-true") or (str(label_list[0]) == "half-true"):
+                        self.my_string_var.set("Scrape Prediction:"+ "real")
+                    else:
+                        self.my_string_var.set("Scrape Prediction:" + "fake")
                     break
                 else:
+                    self.my_string_var.set("Scrape Prediction:" + "not found")
                     print(score)
             # self.my_string_var.set("result3")
 
         self.my_string_var.set("Results")
+        test = ImageTk.PhotoImage(self.image1)
+        self.label1 = ttk.Label(self.mainf, image=test)
+        self.label1.image = test
 
         # create a label widget
         my_label = ttk.Label(self.mainf, textvariable=self.my_string_var)
+        result_label = ttk.Label(self.mainf, text = "scrape result is")
 
         b1 = ttk.Button(self.mainf, text="Train", command=result_1)
         b2 = ttk.Button(self.mainf, text="Predict", command=result_2)
@@ -66,55 +87,17 @@ class Win(tk.Frame):
         b1.grid(row=3, column=1, padx=10, pady=10)
         b2.grid(row=3, column=2, padx=10, pady=10)
         b3.grid(row=3, column=3, padx=10, pady=10)
-        my_label.grid(row=5, column=2, padx=10, pady=20)
+        #result_label.grid(row=5, column=1,  padx = 10, pady=50)
+        my_label.grid(row=5, column=2, pady=60)
+        #label1.grid(row=15, column=0, pady = 0)
+        self.label1.place(x=-45, y=260)
 
 
 
 
-    def colour_btns(self):
-        self.red = ttk.Button(self.mainf, text="red",
-                              command=lambda: self.change_background(
-                                  self.colors[0]))
-        self.green = ttk.Button(self.mainf, text="green",
-                                command=lambda: self.change_background(
-                                    self.colors[1]))
-        self.blue = ttk.Button(self.mainf, text="blue",
-                               command=lambda: self.change_background(
-                                   self.colors[2]))
-        self.orange = ttk.Button(self.mainf, text="orange",
-                                 command=lambda: self.change_background(
-                                     self.colors[3]))
-        self.red.grid(row=0, column=0)
-        self.green.grid(row=1, column=0)
-        self.blue.grid(row=2, column=0)
-        self.orange.grid(row=3, column=0)
 
-    def theme_btns(self):
-        self.alt = ttk.Button(self.mainf, text="alt",
-                              command=lambda: self.change_theme('alt'))
 
-        self.aqua = ttk.Button(self.mainf, text="aqua",
-                               command=lambda: self.change_theme('aqua'))
-        self.clam = ttk.Button(self.mainf, text="clam",
-                               command=lambda: self.change_theme('clam'))
-        self.deflt = ttk.Button(self.mainf, text="default",
-                                command=lambda: self.change_theme('default'))
-        self.classic = ttk.Button(self.mainf, text="classic",
-                                  command=lambda: self.change_theme('classic'))
-        self.alt.grid(row=0, column=1)
-        self.aqua.grid(row=1, column=1)
-        self.clam.grid(row=2, column=1)
-        self.deflt.grid(row=3, column=1)
-        self.classic.grid(row=4, column=1)
 
-    def change_background(self, color):
-        self.mainf.configure(background='{}'.format(color))
-
-    def change_theme(self, theme):
-        """
-        Changes theme of gui. Input is name of theme as string.
-        """
-        self.s.theme_use(theme)
 
 
 root = tk.Tk()
